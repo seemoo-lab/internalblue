364497511018161ab0b7bd56de6ce85c212fa540
Florian Magin <fmagin@users.noreply.github.com>
2020-01-22 18:06:53 +0100

    Allow test module to be tested with unittest by dynamically generating a test suite from trace files

--- tests/all_traces_test.py
+++ tests/all_traces_test.py
@@ -20,7 +20,38 @@ def test_all_traces():
 
 
 
+import unittest
+
+
+def generate_test_suite():
+
+    def generate_test_from_file(core, tracefile):
+        tracepath, cmd = get_trace_path_cmd_tuple(core, tracefile)
+
+        def test():
+            test_trace(core, tracepath, cmd)
+
+        # Rename the function to the tracefile name without .trace suffix
+        test.__name__ = tracefile[:-6]
+        if cmd:
+            return test
+        else:
+            return unittest.skip("No command specified in trace {}".format(tracepath))(test)
+
+    suite = unittest.TestSuite()
+    for core in os.listdir(tracedir):
+        core_suite = unittest.TestSuite()
+        for tracefile in os.listdir(os.path.join(tracedir, core)):
+            if tracefile != '.gitkeep':
+                core_suite.addTest(
+                    unittest.FunctionTestCase(generate_test_from_file(core, tracefile), description=tracefile))
+        suite.addTest(core_suite)
+
+    return suite
+    # test_all_traces()
+
 
 
 if __name__ == '__main__':
-    test_all_traces()
+    suite = generate_test_suite()
+    unittest.TextTestRunner().run(suite)
