064c01ee81a1d143f81e9ccb817f3198fc031c57
Florian Magin <fmagin@users.noreply.github.com>
2020-01-30 13:59:08 +0100

    Remove the old method that manually searched the tests. Replaced with `generate_test_suite` to generate a UnittestSuite

--- tests/all_traces_test.py
+++ tests/all_traces_test.py
@@ -6,17 +6,6 @@ tracedir = os.path.join(os.path.dirname(__file__), "traces")
 
 #cores = ['macoscore', 'ioscore', 'adbcore', 'hcicore']
 
-def test_all_traces():
-    cores = os.listdir(tracedir)
-    for core in cores:
-        for tracefile in os.listdir(os.path.join(tracedir, core)):
-
-                tracepath, cmd = get_trace_path_cmd_tuple(core, tracefile)
-                if cmd:
-                    print("Running trace {} on core {} with cmds {}".format(tracefile, core, cmd))
-                    test_trace(core, tracepath, cmd)
-                else:
-                    print("Skipping trace {} because it has no commands specified".format(tracefile))
 
 
 
