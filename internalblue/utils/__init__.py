edfac4d20977eb0f03386e35d244610edd4f2671
Davide Toldo <davide.toldo@stud.tu-darmstadt.de>
2020-10-08 17:23:09 +0200

    Fix unpack methods

--- internalblue/utils/__init__.py
+++ internalblue/utils/__init__.py
@@ -1,6 +1,7 @@
-# from pwnlib.util.packing import *
 import struct
 from typing import Union
+
+from internalblue import Address
 
 
 def bytes_to_hex(data):
@@ -8,17 +9,51 @@ def bytes_to_hex(data):
     return "".join(format(x, "02x") for x in bytearray(data))
 
 
-def p8(num):
+def p8(num, endian: str = ''):
+    if endian.lower() == 'big':
+        return struct.pack('>B', num)
+    elif endian.lower() == 'little':
+        return struct.pack('<B', num)
     return struct.pack('B', num)
 
 
-def p16(num):
+def p16(num, endian: str = ''):
+    if endian.lower() == 'big':
+        return struct.pack('>H', num)
+    elif endian.lower() == 'little':
+        return struct.pack('<H', num)
     return struct.pack('H', num)
 
 
-def p32(num):
+def u16(num, endian: str = ''):
+    if endian.lower() == 'big':
+        return struct.unpack('>H', num)[0]
+    elif endian.lower() == 'little':
+        return struct.unpack('<H', num)[0]
+    return struct.unpack('H', num)[0]
+
+
+def p32(num, endian: str = ''):
+    if endian.lower() == 'big':
+        return struct.pack('>I', num)
+    elif endian.lower() == 'little':
+        return struct.pack('<I', num)
     return struct.pack('I', num)
 
 
-def u32(num):
-    return struct.unpack('I', num)
+def u32(num, endian: str = ''):
+    if endian.lower() == 'big':
+        return struct.unpack('>I', num)[0]
+    elif endian.lower() == 'little':
+        return struct.unpack('<I', num)[0]
+    return struct.unpack('I', num)[0]
+
+
+def flat(data: [Address, bytes], filler: int) -> bytes:
+    res = bytes()
+    last_section_end = 0
+    for address in data:
+        res += bytes([filler]) * (address - last_section_end)
+        res += data[address]
+        last_section_end = address + len(data[address])
+    return res
